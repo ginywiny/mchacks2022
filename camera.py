27f6c315d9abf690e2af5685240c9376c2857649
ginywiny <michaelles97@gmail.com>
2022-01-23 22:27:09 -0500

    Predict cropped image in camera.py for testing

--- camera.py
+++ camera.py
@@ -2,6 +2,24 @@ from email.mime import image
 import cv2
 import os
 import image_difference
+from torchvision.models import resnet18
+from torchvision import transforms as T
+import torch
+
+# The neural network
+model = resnet18(pretrained=True)
+
+model = resnet18(pretrained=True)
+num_in_features = model.fc.in_features
+
+# replace fully connected layer with one who's output dimension matches the number of classes
+model.fc = torch.nn.Linear(num_in_features, 3)
+
+models_dir = os.path.join(os.path.dirname(__file__), 'models')
+model.load_state_dict(torch.load(os.path.join(models_dir, "model.pth")))
+
+model.eval().cpu()
+items = ["book", "deodorant", "protein_powder"]
 
 def main(show_pics=True):
 
@@ -44,9 +62,17 @@ def main(show_pics=True):
                 bbox = image_difference.predict_removed_object(before_image, frame)
                 cropped_image = image_difference.crop_bbox_from_image(bbox, before_image)
 
+                softmax = torch.nn.Softmax(dim=1)
+                tensor = T.ToTensor()(cropped_image)
+                output = model(tensor.unsqueeze(0))
+                output = softmax(output)
+                print(f"output: {output}")
+                label_index = output.argmax().numpy().item()
+                label = items[label_index]
+
                 if show_pics:
                     cv2.imshow("image_diff", image_diff)
-                    cv2.imshow("cropped_image", cropped_image)
+                    cv2.imshow(f"{label}", cropped_image)
             
             except Exception as e:
                 print(f"could not compute image difference: {e}")
